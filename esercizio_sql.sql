de89c62773b64ee2e5a50f8e52c1e316a6d836a4
VincenzoAmari <vincenzo.am99@gmail.com>
2025-03-11 16:15:31 +0100

    Consegna SQL

---
+++ esercizio_sql.sql
@@ -0,0 +1,50 @@
+CREATE DATABASE db_university;
+/*1. Selezionare tutti gli studenti nati nel 1990 (160)*/
+
+SELECT COUNT(*) AS all_students 
+FROM students
+WHERE YEAR(date_of_birth) = 1990;
+
+
+/* 2. Selezionare tutti i corsi che valgono più di 10 crediti (479)*/
+
+SELECT COUNT(*) AS courses 
+FROM courses
+WHERE cfu > 10;
+
+/* 3. Selezionare tutti gli studenti che hanno più di 30 anni*/
+
+SELECT COUNT(*) AS students_over30
+FROM students
+WHERE YEAR(CURDATE()) - YEAR(date_of_birth) > 30;
+
+/* 4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di
+ laurea (286)*/
+ 
+SELECT COUNT(*) FROM courses 
+WHERE period = "I semestre" 
+AND year = 1;
+
+/* 5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del
+ 20/06/2020 (21)*/
+ 
+SELECT COUNT(*) AS exams
+FROM exams
+WHERE date = '2020-06-20' AND HOUR(hour) >= 14;
+
+/* 6. Selezionare tutti i corsi di laurea magistrale (38)*/
+
+SELECT COUNT(*) AS degrees
+FROM degrees 
+WHERE level = 'Magistrale';
+
+/* 7. Da quanti dipartimenti è composta l'università? (12)*/
+
+SELECT COUNT(*) AS number_departments
+FROM departments;
+
+/* 8. Quanti sono gli insegnanti che non hanno un numero di telefono? (50 */
+
+SELECT COUNT(*) AS insegnanti_senza_telefono 
+FROM teachers 
+WHERE phone IS NULL 
